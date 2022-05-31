56b9d0a14a8ef3948c3b851d62425e11b9e394d1
austintigerlu <austintigerlu@gmail.com>
2022-05-31 14:56:31 -0400

    first

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+
+# Here we will be creating a class called Home and extending it from the View class
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
